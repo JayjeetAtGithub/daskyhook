588cf8de5d30246ae090ed9145837c2f3fb358f7
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-04-07 18:55:05 -0700

    update the install docker script

--- install_docker.sh
+++ install_docker.sh
@@ -5,6 +5,10 @@ node_list=${1}
 
 IFS=', ' read -r -a nodes <<< "$node_list"
 
+# install on client node
+curl -o- https://get.docker.com | bash
+
+# install on cluster nodes
 for node in ${nodes[@]}
 do
 	ssh $node "curl -o- https://get.docker.com | bash"
