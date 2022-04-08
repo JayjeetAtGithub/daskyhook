808f7ce798f50b3ebf134a565ff3184146f73b97
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-04-07 22:36:42 -0700

    add script to install pyarrow

--- install_pyarrow.sh
+++ install_pyarrow.sh
@@ -5,7 +5,9 @@ node_list=${1}
 IFS=',' read -r -a node_list <<< "$node_list"
 
 for node in ${node_list[@]}
-do 
+do
+	ssh $node apt update 
+	ssh $node apt install -y python3-pip 
 	scp /tmp/arrow/python/dist/*.whl $node:/users/noobjc
 	ssh $node pip install /users/noobjc/*.whl
 done
