d17202f58074e3766fe30dba9ac0cb542139074e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-04-10 19:18:23 -0700

    update install dask script

--- install_dask.sh
+++ install_dask.sh
@@ -4,9 +4,11 @@ set -ex
 worker_nodes=${1}
 IFS=',' read -r -a workers <<< "$worker_nodes"
 
-dask-scheduler --interface eno1d1
+pip install dask[distributed,dataframe]
+nohup dask-scheduler --interface eno1d1 &
 
 for worker in ${workers[@]}
 do 
-	ssh $worker "dask-worker --interface eno1d1 tcp://10.10.1.1:8786"
+	ssh $worker "pip install dask[distributed,dataframe]"
+	ssh $worker "nohup dask-worker --interface eno1d1 tcp://10.10.1.1:8786 &"
 done
