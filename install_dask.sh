a0d6f64153c4a957608c715c60e8b34a77e7acbf
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-04-10 19:15:20 -0700

    update install dask script

--- install_dask.sh
+++ install_dask.sh
@@ -4,10 +4,9 @@ set -ex
 worker_nodes=${1}
 IFS=',' read -r -a workers <<< "$worker_nodes"
 
-docker run --privileged -d --restart on-failure --network host -p 8787:8787  ghcr.io/dask/dask dask-scheduler --interface eno1d1  
+dask-scheduler --interface eno1d1
 
 for worker in ${workers[@]}
 do 
-	ssh $worker "docker run --privileged -d --restart on-failure --network host -p 8787:8787  ghcr.io/dask/dask dask-worker --interface eno1d1 tcp://10.10.1.1:8786" 
+	ssh $worker "dask-worker --interface eno1d1 tcp://10.10.1.1:8786"
 done
-
