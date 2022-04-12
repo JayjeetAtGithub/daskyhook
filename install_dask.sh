132219d4ab714a051eeae468828117fc67574b2c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-04-11 22:14:30 -0700

    update install dask script

--- install_dask.sh
+++ install_dask.sh
@@ -8,21 +8,12 @@ rm -rf dask
 git clone https://github.com/uccross/dask
 cd dask; git checkout support-skyhook
 pip install --upgrade .[distributed,dataframe]
-p=$(pidof dask-scheduler)
-if [ -z $p ]; then
-	echo "Dask scheduler is not running"
-	exit 1
-else
-	echo "Dask scheduler is running; killing it"
-	pkill $p
-fi
 
 for worker in ${workers[@]}
 do 
 	ssh $worker "rm -rf dask"
 	ssh $worker "git clone https://github.com/uccross/dask"
 	ssh $worker "cd dask; git checkout support-skyhook; pip install --upgrade .[distributed,dataframe]"
-	ssh $worker "kill -z `pidof dask-worker`" || true
 done
 
 # nohup dask-scheduler --interface eno1d1 &
