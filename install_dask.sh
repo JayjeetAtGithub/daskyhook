a3edd55478fcb89f97cc42ecb9e715f19a34db0e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-04-12 08:39:03 -0700

    update install dask script

--- install_dask.sh
+++ install_dask.sh
@@ -4,7 +4,7 @@ set -ex
 worker_nodes=${1}
 IFS=',' read -r -a workers <<< "$worker_nodes"
 
-kill $(cat /tmp/daskpid) || true
+kill $(pgrep dask-scheduler) || true
 rm -rf dask
 git clone https://github.com/uccross/dask
 cd dask; git checkout support-skyhook
@@ -12,11 +12,11 @@ pip install --upgrade .[distributed,dataframe]
 
 for worker in ${workers[@]}
 do 
-	ssh $worker "kill $(cat /tmp/daskpid) || true"
+	ssh $worker "kill $(pgrep dask-worker) || true"
 	ssh $worker "rm -rf dask"
 	ssh $worker "git clone https://github.com/uccross/dask"
 	ssh $worker "cd dask; git checkout support-skyhook; pip install --upgrade .[distributed,dataframe]"
 done
 
-# nohup dask-scheduler --interface eno1d1 --pid-file /tmp/daskpid &
-# nohup dask-worker --interface eno1d1 tcp://10.10.1.1:8786 --pid-file /tmp/daskpid &
+# nohup dask-scheduler --interface eno1d1 &
+# nohup dask-worker --interface eno1d1 tcp://10.10.1.1:8786 &
