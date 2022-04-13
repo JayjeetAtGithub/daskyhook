fa1101b8ecb822d6abb4a41ac3230748f7e39779
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-04-12 21:27:48 -0700

    add network scripts

--- network.sh
+++ network.sh
@@ -7,12 +7,12 @@ IFS=',' read -r -a node_list <<< "$node_list"
 apt update
 apt install -y wondershaper
 sudo wondershaper clear eno1d1
-sudo wondershaper eno1d1 10240000 10240000
+sudo wondershaper eno1d1 40960000 40960000
 
 for node in ${node_list[@]}
 do	
     ssh $node "apt update"
     ssh $node "apt install -y wondershaper"
     ssh $node "sudo wondershaper clear eno1d1"
-    ssh $node "sudo wondershaper eno1d1 10240000 10240000"
+    ssh $node "sudo wondershaper eno1d1 40960000 40960000"
 done
